ff651bfa923dbd1e87dc1a5cfef7dd637b6edcd7
kvirolainen <zaoozka@gmail.com>
2022-11-29 11:16:57 +0300

    dirs

---
+++ static/index.js
@@ -0,0 +1,50 @@
+const fileInput = document.querySelector("#inputFiles");
+const email = document.querySelector("#inputEmail");
+const prompt = document.querySelector("#inputPrompt");
+//Click on logo will send AJAX post request (instead of sending it from the form)
+const logo = document.querySelector(".logo");
+let isSubmit = false;
+
+//Limit number of files
+//сделать неактивной кнопку сабмит, и активировать ее только если подходит количество файлов
+fileInput.addEventListener("change", (e) => {
+  const fileArray = Array.from(fileInput.files);
+  console.log(fileArray);
+  if (fileArray.length < 15 || fileArray.length > 30) {
+    isSubmit = false;
+    alert("alertalertaler");
+  } else {
+    isSubmit = true;
+  }
+});
+
+//document.addEventListener("DOMContentLoaded", initApp);
+
+//alternative way of sending POST request
+logo.addEventListener("click", sendPhotosToServer);
+
+async function sendPhotosToServer() {
+  if (!isSubmit) {
+    alert("Please check number of your images");
+    return;
+  }
+  let formData = new FormData();
+  formData.append("email", email.value);
+  formData.append("prompt", prompt.value);
+
+  const files = Array.from(fileInput.files);
+
+  files.forEach((file) => {
+    formData.append("files", file);
+  });
+
+  console.log(formData);
+
+  //POST REQUEST TO DO
+  await fetch("https://httpbin.org/post", {
+    method: "POST",
+    body: formData,
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
